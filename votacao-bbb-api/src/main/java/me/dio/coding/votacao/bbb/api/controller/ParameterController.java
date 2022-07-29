e1fb0add842a36a903096284686edd9cd4790ed4
luizsac <lu_que90@hotmail.com>
2022-07-28 23:15:28 -0300

    configures mongodb; creates parameter's model, controller and repository; adds swagger dependency

--- votacao-bbb-api/src/main/java/me/dio/coding/votacao/bbb/api/controller/ParameterController.java
+++ votacao-bbb-api/src/main/java/me/dio/coding/votacao/bbb/api/controller/ParameterController.java
@@ -1,12 +1,37 @@
 package me.dio.coding.votacao.bbb.api.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import lombok.AllArgsConstructor;
+import me.dio.coding.votacao.bbb.api.model.ParameterModel;
+import me.dio.coding.votacao.bbb.api.repository.ParameterRepository;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
 
 @RestController
-@RequestMapping("/api")
+@RequestMapping("/api/parameters")
+@AllArgsConstructor
 public class ParameterController {
 
+    private final ParameterRepository repository;
 
+    @PostMapping()
+    public ResponseEntity<ParameterModel> save(@RequestBody ParameterModel parameter) {
+        ParameterModel entity = repository.save(parameter);
+        return ResponseEntity.ok(entity);
+    }
+
+    @GetMapping()
+    public ResponseEntity<List<ParameterModel>> getAll() {
+        return ResponseEntity.ok(repository.findAll());
+    }
+
+    @GetMapping("/parameter")
+    public ResponseEntity<ParameterModel> get(@RequestParam String key) {
+        Optional<ParameterModel> optParameter = repository.findById(key);
+        if (optParameter.isEmpty()) return ResponseEntity.notFound().build();
+        return ResponseEntity.ok(optParameter.get());
+    }
 
 }
